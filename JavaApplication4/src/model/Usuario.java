cb849f548537d9d40fac43d22229c4e99aacb715
ordrucxs <pedroluizbernado007@gmail.com>
2023-06-05 12:35:45 -0300

    add database

---
+++ JavaApplication4/src/model/Usuario.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    public Usuario UsuarioLogado;
+}
